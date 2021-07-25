b3576e2c0ed49fddc607464163504a416115ea10
ksrisayee12 <ksrisayee@gmail.com>
2021-07-25 22:48:38 +0530

    Created JARVIS v1.0

---
+++ Jarvis.py
@@ -0,0 +1,206 @@
+# Importing Packages
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import os
+import webbrowser
+
+# Creating Listener And Engine
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+# Creating A Speaking Engine
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+# Creating A Speech Recognizer
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'jarvis' in command:
+                command = command.replace('jarvis', '')
+                print(command)
+    except:
+        pass    
+    return command
+
+# Welcome Speech Method
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        talk('good morning')
+    elif hour >= 12 and hour < 18:
+        talk('good afternoon')
+    else:
+        talk('good evening')
+    talk('i am jarvis your personal A  I assistant')
+    talk('how can i help you')
+
+# Main JARVIS Method
+def run_jarvis():
+    command = take_command()
+    print(command)
+    # For Playing Songs On YouTube 
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+
+    # For Telling Time
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%H:%M')
+        print(time)
+        talk('Current time is' + time)
+
+    # Searching Queries On Wikipedia
+    elif 'wikipedia' in command:
+        person = command.replace('wikipedia', '')
+        info = wikipedia.summary(person, 6)
+        print('According to wikipedia')
+        print(info)
+        talk('according to wikipedia')
+        talk(info) 
+
+    # Time For A Joke
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+
+    # Just Fun Commands
+    elif 'how are you' in command:
+        print('i am alright how are you my boss')
+        talk('i am alright how are you my boss')
+
+    elif 'i am fine' in command:
+        print('ok how can i help you')
+        talk('ok how can i help you')
+
+    # Searching Queries On Google
+    elif 'search' in command:
+        result = command.replace('search', '')
+        talk('searching ' + result + 'on google')
+        pywhatkit.search(result)
+
+    # Opening Apps
+    elif 'open vs code' in command:
+        codePath = "C:\\Users\\User\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+        talk('opening visual studio code')
+        os.startfile(codePath)
+
+    elif 'open notepad' in command:
+        notepadPath = "C:\\WINDOWS\\system32\\notepad.exe"
+        talk('opening notepad')
+        os.startfile(notepadPath)
+
+    elif 'open chrome' in command:
+        chromePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        talk('opening chormme')
+        os.startfile(chromePath)
+
+    elif 'open microsoft browser' in command:
+        edgePath = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
+        talk('opening edge browser')
+        os.startfile(edgePath)
+
+    elif 'open firefox' in command:
+        firefoxPath = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"
+        talk('opening firefox')
+        os.startfile(firefoxPath)
+
+    elif 'open command prompt' in command:
+        cmdPath = "C:\\WINDOWS\\system32\\cmd.exe"
+        talk('opening command prompt')
+        os.startfile(cmdPath)
+
+    elif 'open python editor' in command:
+        pycharmPath = "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2021.1.3\\bin\\pycharm64.exe"
+        talk('opening py charm')
+        os.startfile(pycharmPath)
+
+    elif 'open obs studio' in command:
+        obsPath = "C:\\Program Files\\obs-studio\\bin\\64bit\\obs64.exe"
+        talk('opening obs studio')
+        os.startfile(obsPath)
+
+    elif 'open python browser' in command:
+        pythonBrowserPath = "D:\\Srisayee K\\Coding\\A.I PROJECTS\\Speech-Recognition-Projects\\Jarvis-Desktop-Assistant\\Browser.py"
+        talk('opening Python Browser')
+        os.startfile(pythonBrowserPath)
+
+    # Opening Websites
+    elif 'open youtube' in command:
+        talk('opening youtube')
+        webbrowser.open('www.youtube.com')
+
+    elif 'open google' in command:
+        talk('opening google')
+        webbrowser.open('www.google.com')
+
+    elif 'open stackoverflow' in command:
+        talk('opening stackoverflow')
+        webbrowser.open('www.stackoverflow.com')
+
+    elif 'open facebook' in command:
+        talk('opening facebook')
+        webbrowser.open('www.facebook.com')
+
+    elif 'open instagram' in command:
+        talk('opening instagram')
+        webbrowser.open('www.instagram.com')
+
+    elif 'open github' in command:
+        talk('opening github')
+        webbrowser.open('www.github.com')
+
+    elif 'open spotify' in command:
+        talk('opening spotify')
+        webbrowser.open('www.spotify.com')
+        
+    elif 'open linkedin' in command:
+        talk('opening linkedin')
+        webbrowser.open('www.linkedin.com')
+
+    # About Jarvis And The Creator
+    elif 'who is your creator' in command:
+        talk('I was created by sri saiiii')
+        print('I was created by sri sayee')
+
+    elif 'who created you' in command:
+        talk('I was created by sri saiiii')
+        print('I was created by sri sayee')
+
+    elif 'introduce yourself' in command:
+        print('i am jarvis your personal A  I assistant i was created by sri sayee i am a advanced version of A  I alexa created by sri sayee')        
+        talk('i   am   jarvis   your   personal   A   I   assistant   i   was   created   by   sri   saiiii   i   am   a   advanced   version   of   A  I   alexa   created   by   sri   saiiii')        
+        print('i can open apps open youtube videos tell jokes tell time searches your query on wikipedia and google')
+        talk('i   can   open   apps   open   youtube   videos   tell   jokes   tell   time   searches    your    query    on   wikipedia   and   google')
+
+    # Checking if any error in the command and asking for repeating
+    else:
+         talk('Please say that command again')
+
+# Running Wishme function
+if __name__ == '__main__':
+    print('i am jarvis your personal AI assistant')
+    wishme()
+    print('how can i help you')
+
+# Running Jarvis function
+while True:
+    run_jarvis()
+
+    commandLine = take_command()
+
+    # Giving JARVIS a exit command
+    if 'exit' in commandLine:
+        talk('ok sir let me exit')
+        break
---
+++ README.md
@@ -0,0 +1,69 @@
+# Jarvis
+
+In this repository **Jarvis** is a low-level **AI** project which makes :
+
+- A Google Search On What You Say.
+- Tell you some programing jokes.
+- Search videos on YouTube.
+- Tell you time.
+- Tell you some 5 line facts from Wikipedia.
+- Opens you some browser, code editor, cmd(command prompt) obs studio. <br>
+
+Always use the word _**"Jarvis"**_ before you speak to it
+
+## Requirements
+
+To use this _**Jarvis**_ you need to install[ _**Python**_](https://www.python.org/). If you don't have _**Python**_ in your computer click [_**Here To Download**_](https://www.python.org/downloads/).<br>
+
+And the next is to and copy the lines given below and paste it in your **cmd(command prompt)** or **Power Shell** or in a _**terminal**_ in your IDE <br>
+`pip install SpeechRecognition` <br>
+`pip install PyAudio` <br>
+`pip install pyttsx3` <br>
+`pip install pywhatkit` <br>
+`pip install wikipedia` <br>
+`pip install pyjokes` <br>
+`pip install PyQt5` <br>
+`pip install PyQtWebEngine` <br>
+You should also have a mic to speak with _**Jarvis**_
+
+## Commands
+
+To get _**Jarvis**_ in your control use this keywords while asking something to _**Jarvis**_.
+
+- You can use the keyword **Introduce Yourself** so that it will say the features about it.
+- You can use the keyword **who is your creator** or **who created you** to know the name of the creator.
+- To ask **Time** use the keyword _**time**_.
+- To ask Jarvis a **Joke** use the keyword _**joke**_.
+- To ask **The 5 Line Fact From Wikipedia** use the keyword _**wikipedia**_.
+- To **Search Google** use the keyword _**search**_.
+- To **Play A Video On YouTube** use the keyword _**play**_.
+- To **Open Google** use the keyword _**open google**_.
+- To **Open Youtube** use the keyword _**open youtube**_.
+- To **Open Github** use the keyword _**open github**_.
+- To **Open Spotify** use the keyword _**open spotify**_.
+- To **Open Stackoverflow** use the keyword _**open google**_.
+- To **Open Instagram** use the keyword _**open instagram**_.
+- To **Open Facebook** use the keyword _**open facebook**_.
+- To **Open Linkedin** use the keyword _**open linkedin**_.
+
+## Opening Applications
+
+- To open applications use the below keywords: <br>
+  `open vs code` for opening _**Visual Studio Code**._ <br>
+  `open notepad` for opening _**Notepad**._ <br>
+  `open chrome` for opening _**Chrome Browser**._ <br>
+  `open firefox` for opening _**FireFox Browser**._ <br>
+  `open microsoft browser` for opening _**Microsoft Edge Browser**._ <br>
+  `open python editor` for opening _**PyCharm**._ <br>
+  `open obs studio` for opening _**OBS Studio**._ <br>
+  `open command prompt` for opening _**Command Prompt**._ <br>
+  `open python browser` for opening _**A Browser Created By Me**._ <br>
+  ## One Important Thing Before Opening Apps
+  Paste your own application path with double backward slash( \\\ ) in starting of every directory. <br>
+  Or, If you can't understand that following process explained above watch this [video](https://drive.google.com/file/d/1wFB00N8FrITTDrvoGn7UxS1NZQtsMY9s/view?usp=sharing)
+
+## Problems
+
+In some computers while installing `pip install PyAudio` some error will occur. In such case use this [link](https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio) and select the version of your _**Python and the version bit of windows you have**_. It will install a file automatically copy that file and paste it with the _**Jarvis**_ file. And then copy the name of the file you have downloaded and open your **cmd(command prompt)** or **Power Shell** or in a _**terminal**_ in your IDE and use this `pip install`and paste the name of the file you have copied. And by this method this pyaudio problem solved. <br> <br>
+In some computer it may take some time to respond. So, don't take it as an problem <br>
+Pls. have a correctly working mic or it will throw an error
